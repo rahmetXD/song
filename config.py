8f3ffc8067519ad460271f102202a94a8d10128b
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2023-09-27 12:47:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     API_ID = int(os.environ.get("API_ID", "25114508")) #Karışmayın
     API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce") #Karışmayın
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Botunuzun Tokenini Girin .  
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6681715890:AAFPK40dsZ9d1aiPJGPq8-hJ_kgKfseec7E") #Botunuzun Tokenini Girin .  
